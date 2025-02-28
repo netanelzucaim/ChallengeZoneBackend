a74789303f081cdd9e3f8e01c68b6924846d6a99
Noam Moriel <morieln12@gmail.com>
2025-03-01 01:18:32 +0200

    deleted unnecessary marks and logs

--- src/routes/groq_routes.ts
+++ src/routes/groq_routes.ts
@@ -1,10 +1,9 @@
 // routes/chatRoutes.ts
 import express from "express";
-import groqController from "../controllers/groq_controller"; // קריאה לפונקציה מהקונטרולר
+import groqController from "../controllers/groq_controller"; 
 
 const router = express.Router();
 
-// נתיב לפנייה ל-ChatGPT
 router.post("/groq", groqController.getChatResponse);
 
 export default router;
